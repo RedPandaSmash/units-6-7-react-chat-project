e62ef96280102dff9c29c92973ee481482f690ce
RedPandaSmash <redpandasmash@gmail.com>
2025-07-17 21:05:35 -0400

    tyler updated room routes

--- server/routes/room.route.js
+++ server/routes/room.route.js
@@ -4,14 +4,37 @@ import { Router } from "express";
 const router = Router();
 
 //
-router.post("/newroom", (req, res) => {
+router.post("/newroom", async (req, res) => {
   const { name, description, addedUsers } = req.body;
   //
   try {
-    const newRoom = new Room({});
-  } catch (error) {}
+    const newRoom = new Room({
+      name,
+      description,
+      addedUsers,
+    });
+
+    const savedRoom = await newRoom.save();
+
+    res.json({
+      message: "Room made successfully",
+      savedRoom,
+    });
+  } catch (error) {
+    console.error(error);
+  }
 });
 
-// router.get();
+router.get("/", async (req, res) => {
+  try {
+    const rooms = await Rooms.find();
+    res.status(200).json({
+      rooms: rooms,
+      message: "Rooms were found",
+    });
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 export default router;
