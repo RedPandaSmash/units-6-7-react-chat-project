c595a6cc25c72400707dff2a4472e9290033181d
RedPandaSmash <redpandasmash@gmail.com>
2025-08-05 11:59:56 -0400

    little progress

---
+++ client/src/pages/CurrentRoom.jsx
@@ -0,0 +1,6 @@
+//this page will have two parts. a static form
+import React from "react";
+
+export default function CurrentRoom() {
+  return <div>CurrentRoom</div>;
+}
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 // Dashboard corresponds to the default "/" home page, but only displays if the user is logged in with a valid token. This page is not a room you can view and post messages in, but will contain the navigation component that has buttons which link to pages corresponding to existing rooms. If user is not logged in, "/" should redirect to "/register"
-
+// This component is the equivalent of the Rooms Component in the readme
 import React from "react";
 import ExistingRoomsNavigation from "../components/ExistingRoomsNavigation";
 
