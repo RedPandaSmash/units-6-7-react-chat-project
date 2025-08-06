e8869cc7a7a95ebec1295e43461d819d77393b9d
RedPandaSmash <redpandasmash@gmail.com>
2025-08-06 17:39:47 -0400

    forms implemented for signin
    button for conditional rendering functional. register and login forms implemented. need method to set token for authentication and redirection to dashboard.

--- client/src/pages/authentication/Signin.jsx
+++ client/src/pages/authentication/Signin.jsx
@@ -12,11 +12,143 @@ export default function Signin() {
   };
   // Note to create function for when token to authenticate user is assigned, redirect to dashboard. Is this the correct place to do this?
   if (!returningUser) {
-    return <div>Register</div>; /*register form*/
-    //button for "already have an account? Sign in here!" onClick event toggleForms
+    const [form, setForm] = useState({
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+    });
+
+    const handleChange = (e) => {
+      setForm({
+        ...form,
+        [e.target.name]: e.target.value,
+      });
+    };
+
+    const handleSubmit = async (e) => {
+      // prevent default form submission
+      e.preventDefault();
+
+      // send API request to register user/
+      const res = await fetch("http://localhost:8080/api/users/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(form),
+      });
+
+      const data = await res.json();
+
+      alert(data.message);
+
+      // reset form after submission
+      setForm({
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+      });
+    };
+    return (
+      <div>
+        <h1>Register</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="firstName"
+            placeholder="First Name"
+            value={form.firstName}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="lastName"
+            placeholder="Last Name"
+            value={form.lastName}
+            onChange={handleChange}
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={form.email}
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={form.password}
+            onChange={handleChange}
+          />
+          <button type="submit">Register</button>
+        </form>
+        <h2>Already have an account? Click the below button to sign in!</h2>
+        <button onClick={toggleForms}>Sign In</button>
+      </div>
+    );
   }
   if (returningUser) {
-    return <div>Login</div>; /*login form*/
-    //button for "new user? Register here!" onClick event toggleForms
+    const [form, setForm] = useState({
+      email: "",
+      password: "",
+    });
+
+    const handleChange = (e) => {
+      setForm({
+        ...form,
+        [e.target.name]: e.target.value,
+      });
+    };
+
+    const handleSubmit = async (e) => {
+      // prevent default form submission
+      e.preventDefault();
+
+      // send API request to register user/
+      const res = await fetch("http://localhost:8080/api/users/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(form),
+      });
+
+      const data = await res.json();
+
+      alert(data.message);
+
+      // reset form after submission
+      setForm({
+        email: "",
+        password: "",
+      });
+    };
+    return (
+      <div>
+        <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={form.email}
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={form.password}
+            onChange={handleChange}
+          />
+          <button type="submit">Login</button>
+        </form>
+        <h2>New user? Make an account here!</h2>
+        <button onClick={toggleForms}>Sign Up!</button>
+      </div>
+    );
   }
 }
